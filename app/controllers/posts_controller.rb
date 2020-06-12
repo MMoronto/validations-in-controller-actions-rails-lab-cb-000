cfc78dd4f0afa8487c9854cd5652b6447a65e15f
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-06-12 10:24:09 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,10 @@ class PostsController < ApplicationController
   def update
     if @post.update(post_params)
 
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
