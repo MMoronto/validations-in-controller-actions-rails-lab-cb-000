96620a7183f5bda88696bd3c4cdbfa02a093738c
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-06-12 10:22:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,13 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
-    @post = Post.
   end
 
   def edit
   end
 
   def update
-    @post.update(post_params)
+    if @post.update(post_params)
 
     redirect_to post_path(@post)
   end
