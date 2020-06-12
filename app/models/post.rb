561eec87b706db6fdf3518feb9b605d469bbafa8
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-06-12 10:00:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, uniqueness: true
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
